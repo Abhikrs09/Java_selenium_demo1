b7d290b2058d7fb5f199b30ee7de9e7e890211eb
Abhishek Kumar <kabhshek09@gmail.com>
2023-10-08 11:43:59 +0530

    Third commit

--- assignment.bat
+++ assignment.bat
@@ -1,4 +1,4 @@
-set "location= C:\Users\abhishekkumar18\eclipse-workspace\java_selenium_assignment"
+set "location= C:\Users\Kumar\Desktop\Testing\Testing frameworks\java_selenium\java_selenium_assignment"
 call mvn -f %location%/pom.xml dependency:resolve
 call mvn -f %location%/pom.xml compile
 call mvn -f %location%/pom.xml test -DtestSuite=testng
